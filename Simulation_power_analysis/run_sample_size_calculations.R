740becd4888605d9d1af94962dc8a3073202a2c5
psamonwong <phrutsamon.wongnak@gmail.com>
2023-08-31 15:09:24 +0700

    Update run_sample_size_calculations.R

--- Simulation_power_analysis/run_sample_size_calculations.R
+++ Simulation_power_analysis/run_sample_size_calculations.R
@@ -11,6 +11,7 @@ source('priors.R')
 
 # use the linear model fits for simplicity
 load('Rout/model_fits1.RData')
+load('model_settings.RData')
 # get the individual slope estimates
 thetas = extract(out); rm(out)
 my_LOD = 1
@@ -19,43 +20,30 @@ options(mc.cores = ncores)
 
 
 # Set up all simulation settings
-#Trt_effect_pos_control = 1.6
-#Trt_effect_neg_control = 1
-
-#trt_intervention = seq(from=1.2, to=1.6, by=.1)
-#NI_delta = log(1.6)-log(1.4)
-
-Ns = 100 #c(25,50,75,100)
-Nsims = 100
-N_days <- c(2:7)
+Ns = c(25,50,75,100)
+Nsims = 50
+N_days <- c(3:7)
 N_swabs_per_day <- c(2,4)
 
-sim_settings = expand.grid(N=Ns,
+sim_settings = expand.grid(intervention = model_settings$intervention,
+                           ref_arm = model_settings$ref_arm,
+                           N=Ns,
                           sim_k = 1:Nsims,
                           N_days = N_days,
                           N_swabs_per_day = N_swabs_per_day)
-
 
 save(sim_settings, file = 'sim_settings.RData')
 
 ### set up simulation for the settings i
 print(sim_settings[i, ])
-
-#trt_effects = c(sim_settings$trt_control[i],
-#                sim_settings$trt_effect_comp[i])
-#Trt_vector = c(rep(1, sim_settings$N[i]),
-#               rep(2, sim_settings$N[i]))
-
-t_design <- rep(0:sim_settings$N_days[i],sim_settings$N_swabs_per_day[i])
-
+t_design <- sort(c(rep(0,4), rep(1:sim_settings$N_days[i],sim_settings$N_swabs_per_day[i])))
 
 # simulate data
 sim_vl = sim_individuals(thetas = thetas,
                          t_design = t_design,
-                         N = sim_settings$N[i]*2,#length(Trt_vector),
-                         #trt_effects = trt_effects,
+                         N = sim_settings$N[i]*2,
                          Trt_arm = c(rep(1, sim_settings$N[i]), 
-                                     rep(2,sim_settings$N[i])),#Trt_vector,
+                                     rep(2,sim_settings$N[i])),
                          LOD = my_LOD,
                          f_sim = f_sim)
 
@@ -66,12 +54,6 @@ sim_vl = dplyr::arrange(sim_vl, Censored, ID)
 analysis_data=make_stan_inputs(input_data_fit = sim_vl,
                                trt_frmla = as.formula('~Trt'),
                                Dmax = sim_settings$N_days[i]+1)
-
-# xx = aggregate(log10_viral_load~Time+Trt, data = sim_vl, median)
-# ind = xx$Trt==1
-# plot(xx$Time[ind], xx$log10_viral_load[ind],type='l',col='blue',lwd=3,ylim=c(1,6))
-# lines(xx$Time[!ind], xx$log10_viral_load[!ind], col='red', lwd=3)
-
 
 # fit model to simulated data
 mod = stan_model(file = 'Linear_model_basic.stan') # compile
