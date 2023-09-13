8aa57ded98faceef5f205a7fa2dff187b4b77d7e
psamonwong <phrutsamon.wongnak@gmail.com>
2023-09-13 14:08:57 +0700

    Extended simulation analysis

---
+++ Simulation_power_analysis/run_sample_size_calculations_extended.R
@@ -0,0 +1,80 @@
+args = commandArgs(trailingOnly = FALSE) # comes from the SGE_TASKID in *.sh file
+i = as.numeric(args[6])
+print(paste0("job(i) = ", i)) # this will print out in the *.o file
+
+library(rstan)
+library(matrixStats)
+library(doParallel)
+library(stringr)
+source('sample_size_functions.R')
+source('priors.R')
+
+# use the linear model fits for simplicity
+load('Rout/model_fits1.RData')
+load('Rout/model_settings.RData')
+# get the individual slope estimates
+thetas = rstan::extract(out); rm(out)
+my_LOD = 1
+ncores = 4
+options(mc.cores = ncores)
+
+
+# Set up all simulation settings
+Ns = c(25,50,75,100)
+Nsims = 50
+
+day_plans <- c("1,2,3,4,5,6,7", "1,3,5,7", "1,4,7", "1,7", "1,3,5", "1,5")
+
+N_swabs_per_day <- c(2,4)
+
+sim_settings = expand.grid(intervention = model_settings$intervention,
+                           ref_arm = model_settings$ref_arm,
+                           N=Ns,
+                           sim_k = 1:Nsims,
+                           day_plans = day_plans,
+                           N_swabs_per_day = N_swabs_per_day)
+
+save(sim_settings, file = 'sim_settings_extended.RData')
+
+### set up simulation for the settings i
+print(sim_settings[i, ])
+
+day_sampling <- as.numeric(unlist(str_split(sim_settings$day_plans[i], ",")))
+
+t_design <- sort(c(rep(0,4), rep(day_sampling,sim_settings$N_swabs_per_day[i])))
+
+# simulate data
+sim_vl = sim_individuals(thetas = thetas,
+                         t_design = t_design,
+                         N = sim_settings$N[i]*2,
+                         Trt_arm = c(rep(1, sim_settings$N[i]), 
+                                     rep(2,sim_settings$N[i])),
+                         LOD = my_LOD,
+                         f_sim = f_sim)
+
+sim_vl$Trt = factor(sim_vl$Trt_arm,levels=1:2)
+
+sim_vl$Censored = as.numeric(sim_vl$log10_viral_load==sim_vl$log10_cens_vl)
+sim_vl = dplyr::arrange(sim_vl, Censored, ID)
+analysis_data=make_stan_inputs(input_data_fit = sim_vl,
+                               trt_frmla = as.formula('~Trt'),
+                               Dmax = max(t_design)+1)
+
+# fit model to simulated data
+mod = stan_model(file = 'Linear_model_basic.stan') # compile
+stan_out = sampling(mod,
+                    data=c(analysis_data,
+                           all_priors[['WIP']]),
+                    iter=2000,
+                    chain=4,
+                    thin=4,
+                    warmup=1000,
+                    save_warmup = FALSE,
+                    pars=c('trt_effect'),
+                    include=T, verbose=F)
+
+out_trt_effect = as.data.frame(rstan::extract(stan_out, pars='trt_effect'))
+
+f_name = paste0('sims_out/sim_out_extended_',i,'.csv')
+print(f_name)
+write.csv(out_trt_effect, file = f_name,row.names = F)
--- plot_overall_enrollments.R
+++ plot_overall_enrollments.R
@@ -1,8 +1,6 @@
 library(rstan)
-getwd()
+library(RColorBrewer)
 
-mainDir <- "D:/PLATCOV-SAP"
-setwd(mainDir)
 
 data <- read.csv("Analysis_Data/interim_all_analysis.csv")
 str(data)
@@ -15,7 +13,10 @@ data$Trt <- as.factor(data$Trt)
 data$Trt <- factor(data$Trt, levels = c("No study drug", levels(data$Trt)[levels(data$Trt) != "No study drug"]) )
 
 data$Variant <- as.factor(data$Variant)
-data$Variant <- factor(data$Variant, levels = c("Delta", levels(data$Variant)[levels(data$Variant) != "Delta"]))
+data$Variant <- factor(data$Variant, levels = as.character(data$Variant[!duplicated(data$Variant)]))
+
+nb.cols <- length(levels(data$Variant))
+mycolors <- colorRampPalette(brewer.pal(8, "Set1"))(nb.cols)
 
 data$Site <- as.factor(data$Site)
 
@@ -27,12 +28,14 @@ G <- ggplot(data = data, aes(x=Rand_date, y = Trt, col = Variant)) +
   ylab("") +
   geom_vline(xintercept = seq.Date(as.Date("2022-01-01"), as.Date("2024-01-01"), "6 months"),
              col = "red", linetype = "dashed") +
-  scale_x_date(date_labels =  "%b %y") +
-  theme(axis.text.x = element_text(angle = -90, vjust = 0.5, hjust=0),
-        strip.text = element_text(size = 12, face = "bold")) +
-  scale_color_brewer(palette="Set1")
+  scale_x_date(date_labels =  "%b %y", breaks = seq.Date(as.Date("2022-01-01"), as.Date("2024-01-01"), "3 months")) +
+  theme(#axis.text.x = element_text(angle = -90, vjust = 0.5, hjust=0),
+        strip.text = element_text(size = 12, face = "bold"),
+        axis.text = element_text(size = 10)) + 
+  scale_color_manual(values = mycolors) 
+G
 
-pdf("Plots/overall_enrollments.pdf", width = 12, height = 5) 
+pdf("Plots/overall_enrollments_2309.pdf", width = 12, height = 5) 
 G
 dev.off()
 
